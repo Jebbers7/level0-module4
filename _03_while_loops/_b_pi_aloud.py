7e7eb1f7f1c26ab3d7dc7768b7c60c5c8f9c4d33
Jebbers7 <72707609+Jebbers7@users.noreply.github.com>
2022-12-02 18:34:36 -0800

    Update _b_pi_aloud.py

--- _03_while_loops/_b_pi_aloud.py
+++ _03_while_loops/_b_pi_aloud.py
@@ -10,16 +10,25 @@ if __name__ == '__main__':
     window.withdraw()
     # TODO) Place the first 20 digits of pi into a variable as a string
     #  3.1415926535897932384
-
+    pi = "3.1415926535897932384"
     # TODO) Print out the first 3 digits of pi. For example,
     #  pi_str[0]   # first digit
     #  pi_str[1]   # second digit
-
+    for i in range(3):
+        print(pi[i])
     # TODO) Use a while loop to keep asking for the next digit of pi
-
+    correct = True
+    counter = 3
+    while correct:
+        answer = simpledialog.askstring("Pi", "What is the next digit of pi?")
+        if answer == pi[counter]:
+            print("correct")
+            counter += 1
+        else:
+            print("incorrect")
+            correct = False
+    messagebox.showinfo("Pi", "You recited " + str(counter) + " digits of pi in a row!")
         # TODO) If they are correct, print "correct".
         #  If they are not, print "incorrect" and break out of the while loop
-
-
     # TODO) Print out how many digits of pi the user was able to recite
     #  in a row
